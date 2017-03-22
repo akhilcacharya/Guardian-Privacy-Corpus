dcdc62128564f6bcae11e55bf756d5ef9609c67d
Akhil Acharya <akhilcacharya@gmail.com>
2017-03-22 13:00:08 -0400

    Add code to pull text

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const guardian = require('guardian-news'); 
+const request = require('async-request'); 
+const qs = require('querystring'); 
+
+const fs = require('fs'); 
 
 const config = require('./config.json'); 
 
+const PAGE_SIZE = 200 // Max page size
+
 guardian.config({
     apiKey: config.secret_key,
-    pageSize: 50, 
 }); 
 
 const keywords = [
@@ -14,21 +19,66 @@ const keywords = [
 ]
 
 
-async function getArticles(query){
-    return await new Promise((resolve, reject) => {
-        resolve(guardian.content({q: query})); 
+async function getArticlesUrls(query, idx){
+    return await guardian.content({
+            q: query, 
+            pageSize: PAGE_SIZE, 
+            page:idx
     }); 
 }
 
+async function getMetadata(query){
+    return await  guardian.content({
+            q: query, 
+            pageSize: PAGE_SIZE, 
+    }); 
+}
+
+async function fetchArticleText(url){
+    const query = qs.encode({'show-blocks': 'body', 'api-key': config.secret_key}); 
+    const { body } = await request(url + "?" + query); 
+    const { response } = JSON.parse(body); 
+
+    const corpus = response.content.blocks.body.reduce((acc, item) => {
+        return acc += item.bodyTextSummary + "\n"
+    }, ""); 
+
+    return corpus; 
+}
+
+
 async function main(){
-    keywords.forEach(async keyword => {
-        const { response } = await getArticles(keyword); 
 
-        const { pageSize, pages, results } = response; 
+    const urlSet = {}; 
 
-        const titles = results.map(item => item.webTitle); 
+    for(let i = 0; i < keywords.length; i++){
+        const keyword = keywords[i]; 
+        const { response } = await getMetadata(keyword); 
+        let pageIdx = 1
+        let total = response.total
+        console.log(keyword, total)
+    
+        let resultCount = 0; 
+
+        while(total > 0){
+            //Keep increasing the page count
+            const { response } = await getArticlesUrls(keyword, pageIdx); 
+            const { results } = response; 
         
-        console.log(keyword, pages, results.length, "\n", titles);
+            results.forEach(result => {
+                urlSet[result.apiUrl] = true
+                console.log(`${resultCount}/${response.total}`, result.apiUrl)
+                total--; 
+                resultCount++; 
+            }); 
+
+            pageIdx++; 
+        }
+    }; 
+
+
+    Object.keys(urlSet).forEach(async url => {
+        //console.log(fetchArticle(url))
     }); 
 }
 
